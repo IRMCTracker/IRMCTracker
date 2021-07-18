177189ed2b9354e495e587edc1ea60ffe56772f0
Alijk <alijafarkazemi@gmail.com>
2021-07-18 22:53:06 +0430

    Added help command

--- cogs/basics.py
+++ cogs/basics.py
@@ -10,5 +10,20 @@ class Basics(commands.Cog):
     async def ping(ctx):
         await ctx.send("Pong!")
 
+    @commands.command(aliases=["h"])
+    async def help(ctx):
+        embed = discord.Embed(title="🥇 Live Stats category", description="Har 5 daghighe 3 server bartar ro ba tedad player ha neshoon mide", color=0x00D166)
+        await ctx.send(embed=embed)
+
+        embed = discord.Embed(title="💎 All Players", description="Har 5 daghighe majmoo tedad player server haye irani ro neshoon mide", color=0x00D166)
+        await ctx.send(embed=embed)
+
+        embed = discord.Embed(title="📈 Empty Count", description="Tedad server haei ke hich playeri nadaran ro neshoon mide", color=0x00D166)
+        await ctx.send(embed=embed)
+
+        embed = discord.Embed(title="⏰・hourly-tracker", description="Har 1 saat chart tedad player server haye irani ro ersal mikone", color=0x00D166)
+        await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(Basics(bot))
