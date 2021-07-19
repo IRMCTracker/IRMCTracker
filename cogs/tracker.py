8860ef593d41d4e9c4849228c9f47116dc23bb09
Alijk <alijafarkazemi@gmail.com>
2021-07-19 12:50:13 +0430

    Added 4/5/6 top servers voice channels

--- cogs/tracker.py
+++ cogs/tracker.py
@@ -21,6 +21,9 @@ class Tracker(commands.Cog):
                 top1vc = self.bot.get_channel(866289711050784788)
                 top2vc = self.bot.get_channel(866289915783544832)
                 top3vc = self.bot.get_channel(866290014274584606)
+                top4vc = self.bot.get_channel(866594448677928960)
+                top5vc = self.bot.get_channel(866594475110694932)
+                top6vc = self.bot.get_channel(866594488814403615)
 
                 totalvc = self.bot.get_channel(866377410102296596)
                 zerovc = self.bot.get_channel(866377830089621504)
@@ -28,6 +31,9 @@ class Tracker(commands.Cog):
                 await top1vc.edit(name=f"🥇 {sorted_servers[0].get_name()} [{sorted_servers[0].get_online_players()}👥]")
                 await top2vc.edit(name=f"🥈 {sorted_servers[1].get_name()} [{sorted_servers[1].get_online_players()}👥]")
                 await top3vc.edit(name=f"🥉 {sorted_servers[2].get_name()} [{sorted_servers[2].get_online_players()}👥]")
+                await top4vc.edit(name=f"4️⃣ {sorted_servers[3].get_name()} [{sorted_servers[3].get_online_players()}👥]")
+                await top5vc.edit(name=f"5️⃣ {sorted_servers[4].get_name()} [{sorted_servers[4].get_online_players()}👥]")
+                await top6vc.edit(name=f"6️⃣ {sorted_servers[5].get_name()} [{sorted_servers[5].get_online_players()}👥]")
 
                 await totalvc.edit(name=f"💎 All Players [{tracker.all_player_count()}👥]")
                 await zerovc.edit(name=f"📈 Empty Count [{tracker.zero_player_count()}🔨]")
