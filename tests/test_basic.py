4245f84e48a65c5b15333209b62fa94467a3a087
Alijk <alijafarkazemi@gmail.com>
2021-07-26 16:50:02 +0430

    Added comments for tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,11 @@
+"""test.basic.py
+`ONLY` For testings purposes
+
+Can be directly executed via running 'python main.py test' command
+"""
+
 from modules.database import create_tables
 
 create_tables()
-
-# Nothing here :D
-
 
 
