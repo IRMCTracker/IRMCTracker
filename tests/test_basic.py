40ee55438d6df778924d8a5be28b437d37ff9da0
Alijk <alijafarkazemi@gmail.com>
2021-08-03 17:05:02 +0430

    Update imports

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,10 +5,12 @@ Can be directly executed via running 'python main.py test' command
 """
 
 from modules.database import *
+servers = get_servers()
 
 create_tables()
 
-for server in get_servers():
+print(servers[0].name)
+for server in servers:
     print(server.name + ' | ' + str(server.current_players) + ' | ' + str(server.top_players))
 
 
