a1fc3d1b6f33e859288e8e0aad69e54ffcd0555d
Alijk <alijafarkazemi@gmail.com>
2021-08-16 00:47:19 +0430

    Switch models to MySQL format

--- modules/database/basemodel.py
+++ modules/database/basemodel.py
@@ -1,6 +1,9 @@
 from peewee import *
 from modules.database.foundation import database
 
+class LongTextField(TextField):
+    field_type = 'LONGTEXT'
+
 class BaseModel(Model):
     class Meta:
         database = database
--- modules/database/models/server.py
+++ modules/database/models/server.py
@@ -1,14 +1,15 @@
+from typing import Text
 from modules.database.basemodel import *
 
 class Server(BaseModel):
     id = PrimaryKeyField()
-    name = CharField(unique=True)
-    address = CharField(unique=True)
-    discord = CharField(null=True)
-    favicon_path = CharField(null=True)
-    info_path = CharField(null=True)
-    motd_path = CharField(null=True)
-    latest_version = CharField(null=True)
+    name = TextField(unique=True)
+    address = TextField(unique=True)
+    discord = TextField(null=True)
+    favicon_path = LongTextField(null=True)
+    info_path = LongTextField(null=True)
+    motd_path = LongTextField(null=True)
+    latest_version = LongTextField(null=True)
     latest_latency = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
     current_players = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
     top_players = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
