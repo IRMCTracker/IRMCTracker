782c45c5699a2c64a879fa949913f75345b34479
Alijk <alijafarkazemi@gmail.com>
2021-08-29 15:24:38 +0430

    Fix servers meta model

--- modules/database/models/server_meta.py
+++ modules/database/models/server_meta.py
@@ -1,10 +1,11 @@
+from typing import Text
 from modules.database.basemodel import *
 from .server import Server
 
 class ServerMeta(BaseModel):
-    key = LongTextField()
+    key = TextField()
     value = LongTextField()
-    server_id = ForeignKeyField(Server, 'id', backref='votes')
+    server_id = ForeignKeyField(Server, 'id', backref='meta')
 
     class Meta:
         table_name = 'servers_meta'
