c305a2191e60d564ed50823efb8172ba1c71dfd4
Alijk <alijafarkazemi@gmail.com>
2021-09-01 12:21:09 +0430

    Add domain resolving tools

---
+++ modules/utils/domain_tools.py
@@ -0,0 +1,28 @@
+import socket
+from geolite2 import geolite2
+
+class DomainInfo:
+    def __init__(self, domain) -> None:
+        self.domain = domain
+        self._fetch()
+    
+    def _fetch(self):
+        self._fetch_ip()
+        self._fetch_country()
+
+    def _fetch_ip(self):
+        self.ip = socket.gethostbyname(self.domain.strip())
+    
+    def _fetch_country(self):
+        reader = geolite2.reader()      
+        output = reader.get(self.ip)
+        self.country = output['country']['iso_code']
+
+    def get_country(self):
+        return self.country
+    
+    def get_ip(self):
+        return self.ip
+
+    def get_domain(self):
+        return self.domain
