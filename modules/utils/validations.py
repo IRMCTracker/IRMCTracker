7c4b00cefd726755a4d6caf051b6be46d71588ab
Alijk <alijafarkazemi@gmail.com>
2022-01-07 23:49:21 +0330

    Fix moderation malfunction issue

--- modules/utils/validations.py
+++ modules/utils/validations.py
@@ -3,7 +3,7 @@ from discord import Message
 
 def has_link(message):
     regex = re.compile(
-    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:http|ftp)s?:\/\/' # http:// or https://
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
     r'localhost|' #localhost...
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
