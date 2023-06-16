c2ba9c7fd441691ac16e0bd10fe4152ec22624f8
Ali J Kazemi <alijafarkazemi@gmail.com>
2023-06-16 18:09:29 +0330

    Parse channel/guild ids as int

--- modules/config/config_values.py
+++ modules/config/config_values.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:   
-    GUILD_ID = os.getenv('GUILD_ID')
-    SERVER_OF_YEAR_ID = os.getenv('SERVER_OF_YEAR_ID')
+    GUILD_ID = int(os.getenv('GUILD_ID'))
+    SERVER_OF_YEAR_ID = int(os.getenv('SERVER_OF_YEAR_ID'))
 
     class Log:
         DISCORD_DEBUG = bool(os.getenv('LOG_DISCORD_DEBUG'))
@@ -23,32 +23,32 @@ class Config:
         PORT = int(os.getenv('MYSQL_PORT'))
     
     class Channels:
-        TOP_PLAYERS = os.getenv('TOP_PLAYERS').split(", ")
-        TOP_VOTED = os.getenv('TOP_VOTED').split(", ")
+        TOP_PLAYERS = [int(i) for i in os.getenv('TOP_PLAYERS').split(", ")]
+        TOP_VOTED = [int(i) for i in os.getenv('TOP_VOTED').split(", ")]
         
-        RECORD = os.getenv('RECORD')
+        RECORD = int(os.getenv('RECORD'))
         
-        ALL = os.getenv('TOTAL_VC')
-        EMPTY = os.getenv('ZERO_VC')
-        HOURLY = os.getenv('HOURLY_CHART')
-        PIE = os.getenv('PIE_CHART')
+        ALL = int(os.getenv('TOTAL_VC'))
+        EMPTY = int(os.getenv('ZERO_VC'))
+        HOURLY = int(os.getenv('HOURLY_CHART'))
+        PIE = int(os.getenv('PIE_CHART'))
         
-        ADMIN = os.getenv('ADMIN')
+        ADMIN = int(os.getenv('ADMIN'))
 
-        ALERTS = os.getenv('ALERTS')
+        ALERTS = int(os.getenv('ALERTS'))
 
-        CACHE = os.getenv('CACHE')
+        CACHE = int(os.getenv('CACHE'))
 
-        MEMBERS = os.getenv('MEMBERS_COUNT')
-        SERVERS = os.getenv('SERVERS_COUNT')
-        VOTES = os.getenv('VOTES_COUNT')
-        TRACKS = os.getenv('TRACKS_COUNT')
+        MEMBERS = int(os.getenv('MEMBERS_COUNT'))
+        SERVERS = int(os.getenv('SERVERS_COUNT'))
+        VOTES = int(os.getenv('VOTES_COUNT'))
+        TRACKS = int(os.getenv('TRACKS_COUNT'))
 
-        TRACK_USAGE = os.getenv('TRACK_USAGE')
-        PROFILE_USAGE = os.getenv('PROFILE_USAGE')
-        SKIN_USAGE = os.getenv('SKIN_USAGE')
+        TRACK_USAGE = int(os.getenv('TRACK_USAGE'))
+        PROFILE_USAGE = int(os.getenv('PROFILE_USAGE'))
+        SKIN_USAGE = int(os.getenv('SKIN_USAGE'))
 
-        SERVER_OF_YEAR_CHANNEL = os.getenv('SERVER_OF_YEAR')
+        SERVER_OF_YEAR_CHANNEL = int(os.getenv('SERVER_OF_YEAR'))
 
     class Roles:
-        DEFAULT = os.getenv('DEFAULT_ROLE')
+        DEFAULT = int(os.getenv('DEFAULT_ROLE'))
