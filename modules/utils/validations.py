b10e7c281a8edec6dbc4040bb9d02ee702f15909
Ali J Kazemi <alijafarkazemi@gmail.com>
2023-08-20 01:12:18 +0330

    Allow mctracker links

--- modules/utils/validations.py
+++ modules/utils/validations.py
@@ -1,16 +1,30 @@
 import re
 from discord import Message
+from urllib.parse import urlparse
 
 def has_link(message):
-    regex = re.compile(
-    r'(?:http|ftp)s?://' # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-    r'localhost|' #localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-    r'(?::\d+)?' # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    # Define a list of domains to ignore
+    ignored_domains = ["mctracker.ir", "forum.mctracker.ir"]
 
-    return regex.search(message) is not None
+    regex = re.compile(
+        r'(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    match = regex.search(message)
+    if match:
+        url = match.group(0)
+        parsed_url = urlparse.urlparse(url)
+        domain = parsed_url.netloc.lower()
+
+        # Check if the domain is in the list of ignored domains
+        if domain in ignored_domains:
+            return False
+
+    return match is not None
 
 def has_discord_link(message):
     regex = re.compile(
