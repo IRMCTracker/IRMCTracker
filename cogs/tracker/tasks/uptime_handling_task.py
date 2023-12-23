648c746f145dd063e50d2a2737be5609ad060f69
Ali J Kazemi <alijafarkazemi@gmail.com>
2023-12-23 05:56:00 +0330

    Temporary disable uptime alerts task

--- cogs/tracker/tasks/uptime_handling_task.py
+++ cogs/tracker/tasks/uptime_handling_task.py
@@ -19,7 +19,8 @@ class UptimeAlertsTask(Cog):
         
         # Running main bot tick
         self.alert_channel = None
-        self.uptime_alerts_task.start()
+        # Temporary disabling this task
+        # self.uptime_alerts_task.start()
 
     @tasks.loop(minutes=2)
     async def uptime_alerts_task(self):
