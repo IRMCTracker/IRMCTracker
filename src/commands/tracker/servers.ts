b237835c10ba857be9d7540d801450fc271befb5
Ali J. Kazemi <alijafarkazemi@gmail.com>
2024-11-12 17:52:02 +0330

    Make /servers command result paginated

--- src/commands/tracker/servers.ts
+++ src/commands/tracker/servers.ts
@@ -1,83 +1,120 @@
-import { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, APIEmbedField, RestOrArray, MessagePayload } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, APIEmbedField, RestOrArray, MessagePayload, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } from 'discord.js';
 import { Server, getServers } from '../../services/trackerService';
 import { bannerUrl, logoUrl } from "../../config.json";
 import { getMedal, checkChannelPermission } from '../../services/messagingService';
 
 const command: TrackerCommand = {
-	data: new SlashCommandBuilder()
-		.setName('servers')
-		.setDescription('💻 دریافت لیستی از تمام سرور های موجود'),
-	async execute(_, interaction) {
-		if (!await checkChannelPermission(interaction, 'track')) return;
+    data: new SlashCommandBuilder()
+        .setName('servers')
+        .setDescription('💻 دریافت لیستی از تمام سرور های موجود'),
+    async execute(_, interaction) {
+        if (!await checkChannelPermission(interaction, 'track')) return;
         
-		await interaction.reply("🤔 چند لحظه صبر کن...");
+        await interaction.reply("🤔 چند لحظه صبر کن...");
 
-		const servers: Server[] | null = await getServers();
+        const servers: Server[] | null = await getServers();
 
-		if (servers === null) {
-			return await interaction.editReply('🔴 مشکلی در دریافت سرور ها بوجود آمده.');
-		}
+        if (servers === null) {
+            return await interaction.editReply('🔴 مشکلی در دریافت سرور ها بوجود آمده.');
+        }
 
-		const sortedServers = servers.slice().sort((a, b) => {
-			// Sort by players.online (descending)
-			if (b.players.online !== a.players.online) {
-				return b.players.online - a.players.online;
-			}
-			// If players.online is the same, sort by up_from (positive values first)
-			if (a.up_from >= 0 && b.up_from < 0) {
-				return -1; // a comes before b
-			}
-			if (a.up_from < 0 && b.up_from >= 0) {
-				return 1; // b comes before a
-			}
-			// If up_from values are both positive or both negative, sort by their absolute values
-			return Math.abs(b.up_from) - Math.abs(a.up_from);
-		});
-		
+        const sortedServers = servers.slice().sort((a, b) => {
+            // Sort by players.online (descending)
+            if (b.players.online !== a.players.online) {
+                return b.players.online - a.players.online;
+            }
+            // If players.online is the same, sort by up_from (positive values first)
+            if (a.up_from >= 0 && b.up_from < 0) {
+                return -1; // a comes before b
+            }
+            if (a.up_from < 0 && b.up_from >= 0) {
+                return 1; // b comes before a
+            }
+            // If up_from values are both positive or both negative, sort by their absolute values
+            return Math.abs(b.up_from) - Math.abs(a.up_from);
+        });
+        
 
-		const embedFields: RestOrArray<APIEmbedField> = [];
+        const embedFields: RestOrArray<APIEmbedField> = [];
 
-		sortedServers.forEach((server: Server, index: number) => {
-			if (server.up_from > 0) {
-				embedFields.push({ name: `${getMedal(index)} ${server.name}`, value: `👥 ${server.players.online}`, inline: true });
-			} else {
-				embedFields.push({ name: `🔴 ${server.name}`, value: `👥 -`, inline: true });
-			}
-		});
+        sortedServers.forEach((server: Server, index: number) => {
+            if (server.up_from > 0) {
+                embedFields.push({ name: `${getMedal(index)} ${server.name}`, value: `👥 ${server.players.online}`, inline: true });
+            } else {
+                embedFields.push({ name: `🔴 ${server.name}`, value: `👥 -`, inline: true });
+            }
+        });
 
-		const chunkSize = 24;
-		const embeds: EmbedBuilder[] = [];
-		for (let i = 0; i < embedFields.length; i += chunkSize) {
-			const chunk = embedFields.slice(i, i + chunkSize);
+        const chunkSize = 24;
+        const embeds: EmbedBuilder[] = [];
+        for (let i = 0; i < embedFields.length; i += chunkSize) {
+            const chunk = embedFields.slice(i, i + chunkSize);
 			const embed = new EmbedBuilder()
 				.setColor(0x673AB7)
 				.setTimestamp(Date.now())
-				.setFooter({ text: 'Tracked by IRMCTracker' });
+				.setFooter({ text: 'Tracked by IRMCTracker' })
+				.setTitle(`[${Math.floor(i / chunkSize) + 1}/${Math.ceil(embedFields.length / chunkSize)}] 📡 Servers List | لیست سرور ها`)
+				.setImage('attachment://banner.png');
 
-			embed.addFields(chunk);
-			embeds.push(embed);
-		}
+            embed.addFields(chunk);
+            embeds.push(embed);
+        }
+        
+        let currentPage = 0;
 
-		// Setting title on first embed
-		embeds[0].setTitle('📡 Servers List | لیست سرور ها')
+        const row = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('previous')
+                    .setLabel('◀ صفحه قبل')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(true),
+                new ButtonBuilder()
+                    .setCustomId('next')
+                    .setLabel('صفحه بعد ▶')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(embeds.length <= 1)
+            );
 
-		embeds.forEach((embed: EmbedBuilder, index: number) => {
-			if (index != embeds.length - 1) {
-				embeds[index].setThumbnail('attachment://logo.png')
-			}
-		})
-		
-			
+        const response = await interaction.editReply({
+            content: "",
+            embeds: [embeds[0]],
+            files: [{ name: "banner.png", attachment: bannerUrl }],
+            components: [row]
+        });
 
-		// Setting banner/footer on last embed
-		embeds[embeds.length - 1].setImage('attachment://banner.png');
+        // Create a collector for button interactions
+        const collector = response.createMessageComponentCollector({ 
+            componentType: ComponentType.Button,
+            time: 600000 // 10 minutes
+        });
 
-		await interaction.editReply({
-			content: "",
-			embeds: embeds,
-			files: [{ name: "logo.png", attachment: logoUrl }, { name: "banner.png", attachment: bannerUrl }]
-		});
-	},
+        collector.on('collect', async i => {
+            if (i.user.id !== interaction.user.id) {
+                return await i.reply({ content: "You can't use these buttons!", ephemeral: true });
+            }
+
+            if (i.customId === 'previous') {
+                currentPage--;
+            } else if (i.customId === 'next') {
+                currentPage++;
+            }
+
+            // Update button states
+            row.components[0].setDisabled(currentPage === 0);
+            row.components[1].setDisabled(currentPage === embeds.length - 1);
+
+            await i.update({
+                embeds: [embeds[currentPage]],
+                components: [row]
+            });
+        });
+
+        collector.on('end', () => {
+            row.components.forEach(button => button.setDisabled(true));
+            interaction.editReply({ components: [row] }).catch(() => {});
+        });
+    },
 };
 
-export default command
+export default command;
